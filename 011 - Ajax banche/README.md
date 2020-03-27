a417fcc9a6b15b902234db4597d3420ed3dd9b63
Dovetta Nicolas <nicodovetta@gmail.com>
2020-03-27 16:09:31 +0100

    Update
    Update README.md in 011 "- Ajax banche" folder and added the "CREATE DATABASE 4b_banche" instruction to sql file in the "011 - Ajax Banche/doc" folder.

--- 011 - Ajax banche/README.md
+++ 011 - Ajax banche/README.md
@@ -2,3 +2,4 @@
 ## Dovetta Nicolas, 4^B Informatica - I.I.S. "G. Vallauri" Fossano
 
 Simulate, in a small recreation, a National Bank Management.
+The sql file in the doc folder also contains the CREATE DATABASE instructions.
--- 011 - Ajax banche/doc/4b_banche.sql
+++ 011 - Ajax banche/doc/4b_banche.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 -- Database: `dbbanche`
 --
 
+CREATE DATABASE IF NOT EXISTS `4b_banche` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `4b_banche`;
+
 -- --------------------------------------------------------
 
 --
